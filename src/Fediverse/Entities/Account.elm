ed7275e26ead51015a7a081ba656c3e1783b74fb
Vincent Jousse <vincent@jousse.org>
2024-06-30 19:31:01 +0200

    feat: add account encoder and decoder

--- src/Fediverse/Entities/Account.elm
+++ src/Fediverse/Entities/Account.elm
@@ -1,11 +1,12 @@
 module Fediverse.Entities.Account exposing (..)
 
-import Fediverse.Entities.Emoji exposing (Emoji, emojiDecoder)
-import Fediverse.Entities.Field exposing (Field, fieldDecoder)
-import Fediverse.Entities.Role exposing (Role, roleDecoder)
-import Fediverse.Entities.Source exposing (Source, sourceDecoder)
+import Fediverse.Entities.Emoji exposing (Emoji, emojiDecoder, emojiEncoder)
+import Fediverse.Entities.Field exposing (Field, fieldDecoder, fieldEncoder)
+import Fediverse.Entities.Role exposing (Role, roleDecoder, roleEncoder)
+import Fediverse.Entities.Source exposing (Source, sourceDecoder, sourceEncoder)
 import Json.Decode as Decode
 import Json.Decode.Pipeline as Pipe
+import Json.Encode as Encode
 import Time exposing (Posix)
 
 
@@ -74,3 +75,45 @@ accountDecoder =
         |> Pipe.optional "suspended" (Decode.nullable Decode.bool) Nothing
         |> Pipe.required "url" Decode.string
         |> Pipe.required "username" Decode.string
+
+
+accountEncoder : Account -> Encode.Value
+accountEncoder a =
+    Encode.object
+        [ ( "acct", Encode.string a.acct )
+        , ( "avatar", Encode.string a.avatar )
+        , ( "avatarStatic", Encode.string a.avatarStatic )
+        , ( "bot", Encode.bool a.bot )
+        , ( "createdAt", Encode.int (Time.posixToMillis a.createdAt) )
+        , ( "discoverable", a.discoverable |> Maybe.map Encode.bool |> Maybe.withDefault Encode.null )
+        , ( "displayName", Encode.string a.displayName )
+        , ( "emojis", Encode.list emojiEncoder a.emojis )
+        , ( "fields", Encode.list fieldEncoder a.fields )
+        , ( "followersCount", Encode.int a.followersCount )
+        , ( "followingCount", Encode.int a.followingCount )
+        , ( "group", a.group |> Maybe.map Encode.bool |> Maybe.withDefault Encode.null )
+        , ( "header", Encode.string a.header )
+        , ( "headerStatic", Encode.string a.headerStatic )
+        , ( "id", Encode.string a.id )
+        , ( "limited", a.limited |> Maybe.map Encode.bool |> Maybe.withDefault Encode.null )
+        , ( "locked", Encode.bool a.locked )
+        , ( "moved"
+          , a.moved
+                |> Maybe.map
+                    (\ma ->
+                        case ma of
+                            MovedAccount acc ->
+                                accountEncoder acc
+                    )
+                |> Maybe.withDefault Encode.null
+          )
+        , ( "muteExpiresAt", a.muteExpiresAt |> Maybe.map (\m -> Encode.int (Time.posixToMillis m)) |> Maybe.withDefault Encode.null )
+        , ( "noindex", a.noindex |> Maybe.map Encode.bool |> Maybe.withDefault Encode.null )
+        , ( "note", Encode.string a.note )
+        , ( "role", a.role |> Maybe.map roleEncoder |> Maybe.withDefault Encode.null )
+        , ( "source", a.source |> Maybe.map sourceEncoder |> Maybe.withDefault Encode.null )
+        , ( "statusesCount", Encode.int a.statusesCount )
+        , ( "suspended", a.suspended |> Maybe.map Encode.bool |> Maybe.withDefault Encode.null )
+        , ( "url", Encode.string a.url )
+        , ( "username", Encode.string a.username )
+        ]
